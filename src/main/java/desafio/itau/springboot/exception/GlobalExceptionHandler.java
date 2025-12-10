92d4f5147eee211a806194d4381935d4fbf8b614
LucasMedici <lucasrpmedici@gmail.com>
2025-12-09 23:45:44 -0300

    Criando um GlobalExceptionHandler para capturar erros gerados pelo @Valid do validation java seguindo assim os requisitos de UNPROCESSABLE_ENTITY

---
+++ src/main/java/desafio/itau/springboot/exception/GlobalExceptionHandler.java
@@ -0,0 +1,16 @@
+package desafio.itau.springboot.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Void> handleValidationException(MethodArgumentNotValidException exception){
+        return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY).build();
+    }
+}
