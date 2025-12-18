27ab357a7bc2e5dba598f0277d41ea77e3c36734
LucasMedici <lucasrpmedici@gmail.com>
2025-12-18 16:15:54 -0300

    feat: unit test dos services finalizados

---
+++ src/test/java/desafio/itau/springboot/service/StatisticsServiceTest.java
@@ -0,0 +1,74 @@
+package desafio.itau.springboot.service;
+
+import desafio.itau.springboot.model.Transaction;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.time.OffsetDateTime;
+import java.util.Collections;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class StatisticsServiceTest {
+    @InjectMocks
+    private StatisticsService statisticsService;
+
+    @Mock
+    private TransactionService transactionService;
+
+    @Nested
+    class GetStatistics {
+        // lista vazia = valores com 0
+        // conferir valores retorandos sao iguais a valores reais
+
+        @Test
+        void shouldReturnZeroStatisticsWhenNoTransactionsExist(){
+
+            when(transactionService.findByLast60Seconds())
+                    .thenReturn(Collections.emptyList());
+
+            var result = statisticsService.getStatistics();
+
+            assertEquals(0, result.count());
+            assertEquals(0.0, result.sum());
+            assertEquals(0.0, result.avg());
+            assertEquals(0.0, result.min());
+            assertEquals(0.0, result.max());
+            verify(transactionService, times(1)).findByLast60Seconds();
+
+        }
+
+        @Test
+        void shouldReturnCorrectStatisticsValues(){
+            List<Transaction> transactionsList = List.of(
+              new Transaction(20, OffsetDateTime.now()),
+              new Transaction(30, OffsetDateTime.now()),
+              new Transaction(40, OffsetDateTime.now())
+            );
+            when(transactionService.findByLast60Seconds()).thenReturn(transactionsList);
+
+            var result = statisticsService.getStatistics();
+
+            assertEquals(3, result.count());
+            assertEquals(90.0, result.sum());
+            assertEquals(30.0, result.avg());
+            assertEquals(20.0, result.min());
+            assertEquals(40.0, result.max());
+            verify(transactionService, times(1)).findByLast60Seconds();
+
+
+        }
+
+
+
+
+    }
+
+}
---
+++ src/test/java/desafio/itau/springboot/service/TransactionServiceTest.java
@@ -0,0 +1,103 @@
+package desafio.itau.springboot.service;
+
+import desafio.itau.springboot.model.Transaction;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import java.time.OffsetDateTime;
+import java.util.Collections;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+class TransactionServiceTest {
+
+    private TransactionService transactionService;
+
+    @BeforeEach
+    void setup(){
+        transactionService = new TransactionService();
+    }
+
+    @Nested
+    class AddQueue {
+
+        @Test
+        void shouldReturnTransactionFromLast60Seconds(){
+            var transaction = new Transaction(20, OffsetDateTime.now());
+
+            transactionService.addQueue(transaction);
+
+            var listByLast60Seconds = transactionService.findByLast60Seconds();
+
+            assertEquals(1, listByLast60Seconds.size());
+            assertTrue(listByLast60Seconds.contains(transaction));
+
+        }
+
+        @Test
+        void shouldNotReturnTransactionOlderThan60Seconds(){
+            var transaction = new Transaction(20, OffsetDateTime.now().minusSeconds(500));
+
+            transactionService.addQueue(transaction);
+
+            var listByLast60Seconds = transactionService.findByLast60Seconds();
+
+            assertFalse(listByLast60Seconds.contains(transaction));
+        }
+
+        @Test
+        void shouldReturnEmptyList(){
+            var listByLast60Seconds = transactionService.findByLast60Seconds();
+            assertTrue(listByLast60Seconds.isEmpty());
+        }
+    }
+
+    @Nested
+    class ClearQueue {
+
+        @Test
+        void shouldClearAllTransactionsFromQueue(){
+            var transaction1 = new Transaction(20, OffsetDateTime.now());
+            var transaction2 = new Transaction(40, OffsetDateTime.now());
+
+            transactionService.addQueue(transaction1);
+            transactionService.addQueue(transaction2);
+            transactionService.clearQueue();
+            var listByLast60Seconds = transactionService.findByLast60Seconds();
+
+            assertTrue(listByLast60Seconds.isEmpty());
+
+
+        }
+    }
+
+    @Nested
+    class FindByLast60Seconds {
+
+        @Test
+        void shouldReturnOnlyTransactionsFromLast60Seconds(){
+            var t1 = new Transaction(10, OffsetDateTime.now());
+            var t2 = new Transaction(20, OffsetDateTime.now().minusSeconds(20));
+            var t3 = new Transaction(30, OffsetDateTime.now().minusSeconds(40));
+            var t4 = new Transaction(4, OffsetDateTime.now().minusSeconds(70));
+            var t5 = new Transaction(4, OffsetDateTime.now().minusSeconds(120));
+
+            transactionService.addQueue(t1);
+            transactionService.addQueue(t2);
+            transactionService.addQueue(t3);
+            transactionService.addQueue(t4);
+            transactionService.addQueue(t5);
+            var listByLast60Seconds = transactionService.findByLast60Seconds();
+
+            assertTrue(listByLast60Seconds.contains(t1));
+            assertTrue(listByLast60Seconds.contains(t2));
+            assertTrue(listByLast60Seconds.contains(t3));
+            assertFalse(listByLast60Seconds.contains(t4));
+            assertFalse(listByLast60Seconds.contains(t5));
+        }
+
+    }
+
+}
