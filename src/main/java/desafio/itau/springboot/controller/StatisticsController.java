37d320a775a1cf76860c9e802896d32764c0b605
LucasMedici <lucasrpmedici@gmail.com>
2025-12-30 19:08:22 -0300

    feat: implementando lib da openApi e SwaggerUi e configurando a documentação dos endpoints

--- src/main/java/desafio/itau/springboot/controller/StatisticsController.java
+++ src/main/java/desafio/itau/springboot/controller/StatisticsController.java
@@ -2,6 +2,10 @@ package desafio.itau.springboot.controller;
 
 import desafio.itau.springboot.dto.StatisticsDTO;
 import desafio.itau.springboot.service.StatisticsService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("estatistica")
+@Tag(name = "Estatísticas", description = "Operações relacionadas a estatísticas")
 public class StatisticsController {
 
     private StatisticsService statisticsService;
@@ -18,6 +23,8 @@ public class StatisticsController {
         this.statisticsService = statisticsService;
     }
 
+    @Operation(summary = "Busca estatísticas das transações nos últimos segundos configurados", method = "GET")
+    @ApiResponse(responseCode = "200", description = "Dados das estatísticas conforme tempo configurado")
     @GetMapping
     public ResponseEntity<StatisticsDTO> getStatistics(){
         return ResponseEntity.status(HttpStatus.OK).body(statisticsService.getStatistics());
--- src/main/java/desafio/itau/springboot/controller/TransactionController.java
+++ src/main/java/desafio/itau/springboot/controller/TransactionController.java
@@ -3,6 +3,10 @@ package desafio.itau.springboot.controller;
 import desafio.itau.springboot.dto.TransactionDTO;
 import desafio.itau.springboot.model.Transaction;
 import desafio.itau.springboot.service.TransactionService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/transacao")
+@Tag(name = "Transações", description = "Operações relacionadas a transações")
 public class TransactionController {
 
     private TransactionService transactionService;
@@ -18,12 +23,20 @@ public class TransactionController {
         this.transactionService = transactionService;
     }
 
+    @Operation(summary = "Adiciona uma transação na queue", method = "POST")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "A transação foi aceita"),
+            @ApiResponse(responseCode = "422", description = "A transação não foi aceita"),
+            @ApiResponse(responseCode = "400", description = "A API não compreendeu a requisição do cliente")
+    })
     @PostMapping
     public ResponseEntity<Void> addTransaction(@RequestBody @Valid TransactionDTO transactionDTO){
         transactionService.addQueue(new Transaction(transactionDTO.valor(), transactionDTO.dataHora()));
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
+    @Operation(summary = "Limpa a fila de transações", method = "DELETE")
+    @ApiResponse(responseCode = "200", description = "A fila foi limpa com sucesso")
     @DeleteMapping
     public ResponseEntity<Void> clearTransaction(){
         transactionService.clearQueue();
